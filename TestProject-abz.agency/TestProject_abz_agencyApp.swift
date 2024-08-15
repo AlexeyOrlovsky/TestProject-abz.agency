a6a444fb01beb9d7b21e50f5437d04c9e06a67ea
AlexeyOrlovsky <orlovskyiosdev@gmail.com>
2024-08-15 14:54:22 +0300

    project init

---
+++ TestProject-abz.agency/TestProject_abz_agencyApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestProject_abz_agencyApp.swift
+//  TestProject-abz.agency
+//
+//  Created by Алексей Орловский on 15.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct TestProject_abz_agencyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
