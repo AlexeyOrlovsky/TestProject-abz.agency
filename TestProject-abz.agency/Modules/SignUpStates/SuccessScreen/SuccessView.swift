5d0440d00f0c225c14316f887d167687c1f70fc9
Alexey <144032046+AlexeyOrlovsky@users.noreply.github.com>
2024-08-20 12:49:23 +0300

    feature: implemented UI part sign-up-Success screen (#3)

---
+++ TestProject-abz.agency/Modules/SignUpStates/SuccessScreen/SuccessView.swift
@@ -0,0 +1,76 @@
+//
+//  SuccessView.swift
+//  TestProject-abz.agency
+//
+//  Created by Алексей Орловский on 20.08.2024.
+//
+
+import SwiftUI
+
+private typealias Module = SuccessModule
+private typealias ModuleView = Module.MainView
+
+// MARK: - MainView
+extension Module {
+    struct MainView<ViewModel: ViewModelProtocol>: View {
+        // MARK: - Dependencies
+        @StateObject var viewModel: ViewModel
+        @EnvironmentObject var navigator: AppFlowNavigator
+
+        // MARK: - Body
+        var body: some View {
+            content()
+                .onAppear(perform: self.viewModel.onAppear)
+        }
+    }
+}
+
+// MARK: - Private Layout
+private extension ModuleView {
+    @ViewBuilder func content() -> some View {
+        ZStack {
+                StatusView(
+                    image: Image("signUpSuccess"),
+                    message: "User successfully registered",
+                    buttonText: "Got it",
+                    buttonAction: {
+                        debugPrint("Got it")
+                    }
+                )
+                .padding()
+
+                VStack {
+                    HStack {
+                        Spacer()
+                        closeButton()
+                    }
+                    .padding(.top, 29)
+                    .padding(.trailing, 29)
+                    
+                    Spacer()
+                }
+            }
+    }
+    
+    @ViewBuilder func closeButton() -> some View {
+        Button {
+            //
+        } label: {
+            Image("close")
+        }
+
+    }
+}
+
+// MARK: - Private Methods
+private extension ModuleView {
+}
+
+// MARK: - Previews
+#if !RELEASE
+struct SuccessView_Previews: PreviewProvider {
+    static var previews: some View {
+        SuccessModule().assemble()
+    }
+}
+#endif
---
+++ TestProject-abz.agency/Modules/SignUpStates/SuccessScreen/SuccessViewModel.swift
@@ -0,0 +1,43 @@
+//
+//  SuccessViewModel.swift
+//  TestProject-abz.agency
+//
+//  Created by Алексей Орловский on 20.08.2024.
+//
+
+import Foundation
+
+private typealias Module = SuccessModule
+private typealias ViewModel = Module.ViewModel
+
+// MARK: - ViewModel
+extension Module {
+    final class ViewModel: ViewModelProtocol {
+        // MARK: - Public Properties
+
+        // MARK: - Private Properties
+
+        // MARK: - Services
+
+        // MARK: - Managers
+
+        // MARK: - Helpers
+        private var cancellable: CancelBag = .init()
+
+        // MARK: - Init
+        init(
+        ) {
+        }
+
+        // MARK: - Lifecycle
+        func onAppear() {
+            cancellable.cancel()
+        }
+
+        // MARK: - Tap Actions
+    }
+}
+
+// MARK: - Private Methods
+private extension ViewModel {
+}
